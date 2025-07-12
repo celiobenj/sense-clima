92e99a9b4cc1edd28338b9176cb073b58f270aeb
celiob_ <celiolimabenjamim@gmail.com>
2025-07-11 21:50:07 -0400

    test serial

--- Firmware/Applications/SenseClima/Src/main.c
+++ Firmware/Applications/SenseClima/Src/main.c
@@ -5,6 +5,8 @@
 #include "FreeRTOS.h"
 #include "main.h"
 
+extern USART_HandleTypeDef huart1;
+
 /**
   \fn          static void appInit(void *arg)
   \brief
@@ -12,8 +14,16 @@
 */
 static void appInit(void *arg)
 {
-   // add user defined init code
+    DHT22_Init();
+    float temp = 0.0f, hum = 0.0f;
 
+    // add user defined init code
+    while(1)
+    {
+      DHT22_Read(&temp, &hum);
+      printf("temp:%f\nhum:%f\n", temp, hum);
+      vTaskDelay(portTICK_PERIOD_MS * 1000);
+    }
     return;
 }
 
@@ -24,7 +34,13 @@ static void appInit(void *arg)
 */
 void main_entry(void)
 {
+    uint32_t uart_cntrl = (ARM_USART_MODE_ASYNCHRONOUS | ARM_USART_DATA_BITS_8 | ARM_USART_PARITY_NONE | 
+                                ARM_USART_STOP_BITS_1 | ARM_USART_FLOW_CONTROL_NONE);
+
     BSP_CommonInit();
+    
+    HAL_USART_InitPrint(&huart1, GPR_UART1ClkSel_26M, uart_cntrl, 115200);
+
     osKernelInitialize();
     registerAppEntry(appInit, NULL);
     if (osKernelGetState() == osKernelReady)
